c5ab741e949692e6b2adfaef032fee8fc27b6023
ShihabPalekar <shihabpalekar23@gmail.com>
2022-03-09 13:21:03 +0530

    added pagination logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import PostsTable from './components/PostsTable'
+import React, { useEffect, useState } from "react";
+import PostsTable from "./components/PostsTable";
 
-const App : React.FC = () => {
-  const [data, setData]:any = useState([])
-  const [page, setPage] = useState(1)
-  const [storedData, setStoredData] : any = useState({})
+const App: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [storedData, setStoredData]: any = useState({});
 
-  const fetchTableData = async (page:number) => {
-    const response = await fetch(`https://hn.algolia.com/api/v1/search_by_date?query=story&page=${page}`)
-    const resData = await response.json()
-    // setData((prevData: any)=> [...resData.hits, ...prevData])
-    if (storedData[page] === undefined){
-      setStoredData({...storedData, [page]: resData.hits})
+  const fetchTableData = async (page: number) => {
+    const response = await fetch(
+      `https://hn.algolia.com/api/v1/search_by_date?query=story&page=${page}`
+    );
+    const resData = await response.json();
+    if (storedData[page] === undefined) {
+      setStoredData({ ...storedData, [page]: resData.hits });
     }
-  }
+  };
 
-  console.log(storedData)
+  useEffect(() => {
+    fetchTableData(page);
+  }, [page]);
 
-  useEffect(()=>{
-    fetchTableData(page)
-  },[page])
-  
   return (
     <div className="app">
-      <PostsTable data={data}/>
+      <PostsTable storedData={storedData} page={page} setPage={setPage} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/PostsTable.tsx
+++ src/components/PostsTable.tsx
@@ -1,5 +1,6 @@
 import { DataGrid } from "@mui/x-data-grid";
-import { v4 as uuidv4 } from 'uuid';
+import { Pagination } from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
 
 const columns = [
   { field: "title", headerName: "Title", width: 500, sortable: false },
@@ -9,32 +10,51 @@ const columns = [
 ];
 
 type Props = {
-  data: Array<any>
-}
+  storedData: any;
+  page: number;
+  setPage: any;
+};
 
-const PostsTable : React.FC<Props> = ({ ...props }) => {
+const PostsTable: React.FC<Props> = ({ ...props }) => {
+  const handlePageChange = (event: any, value: number) => {
+    props.setPage(value);
+    console.log(value);
+  };
+
   return (
     <div>
-      {props.data === undefined ? (
-        <p>fetching data...</p>
+      {props.storedData[props.page] === undefined ? (
+        <p className="loader">fetching data...</p>
       ) : (
-        <div className="table-wrapper" style={{ height: "80vh", width: "98vw" }}>
-          <DataGrid
-            rows={
-                props.data.map((item: any) => {
-                    return {
-                      id: uuidv4(),
-                      title: item.story_title || "",
-                      author: item.author || "",
-                      createdAt: item.created_at || "",
-                      url: item.story_url || "",
-                    };
-                  })
-            }
-            columns={columns}
-            pageSize={20}
-            rowsPerPageOptions={[20]}
-          />
+        <div>
+          <div
+            className="table-wrapper"
+            style={{ height: "80vh", width: "98vw" }}
+          >
+            <DataGrid
+              rows={props.storedData[props.page].map((item: any) => {
+                return {
+                  id: uuidv4(),
+                  title: item.story_title || "-",
+                  author: item.author || "-",
+                  createdAt: item.created_at || "-",
+                  url: item.story_url || "-",
+                };
+              })}
+              columns={columns}
+              pageSize={20}
+              rowsPerPageOptions={[20]}
+            />
+          </div>
+          <div>
+            <Pagination
+              className="pagination"
+              count={50}
+              page={props.page}
+              color="primary"
+              onChange={handlePageChange}
+            />
+          </div>
         </div>
       )}
     </div>
